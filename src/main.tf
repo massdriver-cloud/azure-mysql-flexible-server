ec69346cddd6f506411af4ea5b4782a2e7604d91
mclacore <michael@massdriver.cloud>
2022-06-14 12:49:20 -0600

    Fixed storage and replaced sku map

--- src/main.tf
+++ src/main.tf
@@ -1,29 +1,3 @@
-locals {
-  size_map = {
-    "B1ms (1 vCore, 2 GiB memory, 640 max iops)"        = "B_Standard_B1ms"
-    "B2s (2 vCores, 4 GiB memory, 1280 max iops)"       = "B_Standard_B2s"
-    "B2ms (2 vCores, 8 GiB memory, 1780 max iops)"      = "B_Standard_B2ms"
-    "B4ms (4 vCores, 16 GiB memory, 2400 max iops)"     = "B_Standard_B4ms"
-    "B8ms (8 vCores, 32 GiB memory, 3100 max iops)"     = "B_Standard_B8ms"
-    "B16ms (16 vCores, 64 GiB memory, 4300 max iops)"   = "B_Standard_B16ms"
-    "D2ds (2 vCores, 8 GiB memory, 3200 max iops)"      = "GP_Standard_D2ds_v4"
-    "D4ds (4 vCores, 16 GiB memory, 6400 max iops)"     = "GP_Standard_D4ds_v4"
-    "D8ds (8 vCores, 32 GiB memory, 12800 max iops)"    = "GP_Standard_D8ds_v4"
-    "D16ds (16 vCores, 64 GiB memory, 18000 max iops)"  = "GP_Standard_D16ds_v4"
-    "D32ds (32 vCores, 128 GiB memory, 18000 max iops)" = "GP_Standard_D32ds_v4"
-    "D48ds (48 vCores, 192 GiB memory, 18000 max iops)" = "GP_Standard_D48ds_v4"
-    "D64ds (64 vCores, 256 GiB memory, 18000 max iops)" = "GP_Standard_D64ds_v4"
-    "E2ds (2 vCores, 16 GiB memory, 3200 max iops)"     = "MO_Standard_E2ds_v4"
-    "E4ds (4 vCores, 32 GiB memory, 6400 max iops)"     = "MO_Standard_E4ds_v4"
-    "E8ds (8 vCores, 64 GiB memory, 12800 max iops)"    = "MO_Standard_E8ds_v4"
-    "E16ds (16 vCores, 128 GiB memory, 18000 max iops)" = "MO_Standard_E16ds_v4"
-    "E32ds (32 vCores, 256 GiB memory, 18000 max iops)" = "MO_Standard_E32ds_v4"
-    "E48ds (48 vCores, 384 GiB memory, 18000 max iops)" = "MO_Standard_E48ds_v4"
-    "E64ds (64 vCores, 432 GiB memory, 18000 max iops)" = "MO_Standard_E64ds_v4"
-  }
-  sku = lookup(local.size_map, var.size, "")
-}
-
 resource "random_password" "master_password" {
   length      = 16
   special     = false
@@ -76,9 +50,8 @@ resource "azurerm_mysql_flexible_server" "main" {
 
   storage {
     size_gb = var.storage_gb
-    iops    = var.iops
   }
-  sku_name = local.sku
+  sku_name = var.sku_name
 
   depends_on = [
     azurerm_private_dns_zone_virtual_network_link.main
